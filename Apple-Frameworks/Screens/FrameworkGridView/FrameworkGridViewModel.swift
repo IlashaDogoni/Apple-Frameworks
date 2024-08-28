37c8259b634c21d6953125624154777814304662
IlashaDogoni <fistonfire@mail.ru>
2024-08-28 21:43:43 +0600

    Some refactoring

--- Apple-Frameworks/Screens/FrameworkGridView/FrameworkGridViewModel.swift
+++ Apple-Frameworks/Screens/FrameworkGridView/FrameworkGridViewModel.swift
@@ -6,14 +6,17 @@
 //
 
 import Foundation
+import SwiftUI
 
 final class FrameworkGridViewModel: ObservableObject{
     
     var selectedFramework: Framework?{
-        didSet{
-            isShowingDetailView = true
-        }
+        didSet{ isShowingDetailView = true }
     }
     
     @Published var isShowingDetailView = false
+    
+    let columns: [GridItem] = [GridItem(.flexible()),
+                               GridItem(.flexible()),
+                               GridItem(.flexible())]
 }
